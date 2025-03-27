91a3153441446ac393a1b3ed8d43973fe47c3b8f
BinhIT <binhtm3@gmail.com>
2025-03-27 15:19:58 +0700

    - Update Flutter 3.29.x

--- android/src/main/kotlin/seo/dongu/heic_to_jpg/HeicToJpgPlugin.kt
+++ android/src/main/kotlin/seo/dongu/heic_to_jpg/HeicToJpgPlugin.kt
@@ -16,36 +16,26 @@ class HeicToJpgPlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         applicationContext = flutterPluginBinding.applicationContext
-        val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "heic_to_jpg")
+        val channel =
+            MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "heic_to_jpg")
         channel.setMethodCallHandler(HeicToJpgPlugin());
     }
 
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
     companion object {
         var applicationContext: Context? = null
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "heic_to_jpg")
-            channel.setMethodCallHandler(HeicToJpgPlugin())
-        }
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
         if (call.method == "convert") {
-            if (call.hasArgument("heicPath") && !call.argument<String>("heicPath").isNullOrEmpty()) {
+            if (call.hasArgument("heicPath") && !call.argument<String>("heicPath")
+                    .isNullOrEmpty()
+            ) {
                 val handler = Handler(Looper.getMainLooper())
                 Thread {
                     var jpgPath = call.argument<String>("jpgPath")
-                    if(jpgPath.isNullOrEmpty()){
-                        jpgPath = "${applicationContext?.cacheDir}/${System.currentTimeMillis()}.jpg"
+                    if (jpgPath.isNullOrEmpty()) {
+                        jpgPath =
+                            "${applicationContext?.cacheDir}/${System.currentTimeMillis()}.jpg"
                     }
                     val output = convertHeicToJpeg(call.argument<String>("heicPath")!!, jpgPath)
                     handler.post {
